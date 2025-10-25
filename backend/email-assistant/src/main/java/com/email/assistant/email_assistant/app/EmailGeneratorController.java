15dc6ed85b8e1fa27e83d4d959c5b5e454cc1a49
chirayu-007 <chirayur789@gmail.com>
2025-10-25 11:17:01 +0530

    Initial commit

---
+++ backend/email-assistant/src/main/java/com/email/assistant/email_assistant/app/EmailGeneratorController.java
@@ -0,0 +1,20 @@
+package com.email.assistant.email_assistant.app;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/email")
+@CrossOrigin(origins = {"http://localhost:4200", "https://mail.google.com"})
+@AllArgsConstructor
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ backend/email-assistant/src/main/java/com/email/assistant/email_assistant/app/EmailRequest.java
@@ -0,0 +1,10 @@
+package com.email.assistant.email_assistant.app;
+
+import lombok.Data;
+
+@Data
+public class EmailRequest {
+
+    private String emailContent;
+    private  String tone;
+}
